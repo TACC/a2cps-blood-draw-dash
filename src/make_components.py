947f69601d1f33eae253691931df77aaedfabbe7
Lissa Pearson <lissa.pearson@gmail.com>
2021-11-05 15:58:54 -0500

    Add deviations and Hemolysis figures

--- src/make_components.py
+++ src/make_components.py
@@ -25,11 +25,15 @@ from styling import *
 # ----------------------------------------------------------------------------
 
 def build_datatable(df,table_id):
-    table = dt.DataTable(
-        id=table_id,
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-    )
+    table = html.Div([
+        dt.DataTable(
+            id=table_id,
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'),
+            style_table={'overflowX': 'auto'},
+            page_size=10
+        )
+        ],style={'margin-bottom':'50px'})
     return table
 
 # ----------------------------------------------------------------------------
@@ -56,47 +60,61 @@ def make_missing(df):
 # ----------------------------------------------------------------------------
 # Sample counts by site
 # ----------------------------------------------------------------------------
+def make_site(df):
+    site = html.Div([
+    dcc.Markdown('''
+    **Sample Counts by Site**
+    * Barplot showing counts by site (parallel bars) and by timepoint (groups of bars) – DONE
+    * Barplot showing percent of samples by site/timepoint with PaxGene obtained
+    * Barplot showing percent of samples by site/timepoint with BuffyCoat obtained
+    * Barplot showing percent of samples by site/timepoint with at least one aliquot tube obtained
+    * Barplot showing percent of samples by site/timepoint with at least five aliquot tubes obtained
+    * Barplot showing distribution of counts of aliquot tubes, faceted by site/timepoint (e.g., num sites rows by num timepoints columns, similar to Aliquots Collected plot from current version, but I think I'd prefer sites on separate panels)
 
-site = dcc.Markdown('''
-**Sample Counts by Site**
-* Barplot showing counts by site (parallel bars) and by timepoint (groups of bars) – DONE
-* Barplot showing percent of samples by site/timepoint with PaxGene obtained
-* Barplot showing percent of samples by site/timepoint with BuffyCoat obtained
-* Barplot showing percent of samples by site/timepoint with at least one aliquot tube obtained
-* Barplot showing percent of samples by site/timepoint with at least five aliquot tubes obtained
-* Barplot showing distribution of counts of aliquot tubes, faceted by site/timepoint (e.g., num sites rows by num timepoints columns, similar to Aliquots Collected plot from current version, but I think I'd prefer sites on separate panels)
+    Flags: Pull complete data for all samples with no PaxGene OR no BuffyCoat OR no aliquot tubes
 
-Flags: Pull complete data for all samples with no PaxGene OR no BuffyCoat OR no aliquot tubes
-
-Purpose: Track number of samples by site, getting a full picture of the types of tubes collected, percent of samples with adequate data collected for analysis
-    '''
-    ,style={"white-space": "pre"}),
-
+    Purpose: Track number of samples by site, getting a full picture of the types of tubes collected, percent of samples with adequate data collected for analysis
+        '''
+        ,style={"white-space": "pre"}),
+        ])
+    return site
 # ----------------------------------------------------------------------------
 # Timing
 # ----------------------------------------------------------------------------
+def make_timing(df):
+    timing = html.Div([
+        dcc.Markdown('''
+    **Timing Data**
+    * Barplots of counts of samples by time to centrifuge, faceted by site (I think it is a little too hard to see easily with all sites on the same plot)
+    * Barplots of counts of samples by time to freezer, faceted by site
 
-timing =  dcc.Markdown('''
-**Timing Data**
-* Barplots of counts of samples by time to centrifuge, faceted by site (I think it is a little too hard to see easily with all sites on the same plot)
-* Barplots of counts of samples by time to freezer, faceted by site
+    * Table with percent of samples by site/timepoint where time to centrifuge <= 30mins
+    * Table with percent of samples by site/timepoint where time to freezer <= 60mins
 
-* Table with percent of samples by site/timepoint where time to centrifuge <= 30mins
-* Table with percent of samples by site/timepoint where time to freezer <= 60mins
+    Flags: Pull complete data for all samples with time to centrifuge > 30mins OR time to freezer > 60 mins OR time to freezer < time to centrifuge
 
-Flags: Pull complete data for all samples with time to centrifuge > 30mins OR time to freezer > 60 mins OR time to freezer < time to centrifuge
-
-Purpose: Check that protocols are being followed to ensure high quality blood samples
-    '''
-    ,style={"white-space": "pre"}),
-
+    Purpose: Check that protocols are being followed to ensure high quality blood samples
+        '''
+        ,style={"white-space": "pre"}),
+        ])
+    return timing
 # ----------------------------------------------------------------------------
 # Hemolysis
 # ----------------------------------------------------------------------------
+def make_hemolysis_fig(hem_df, site):
+    fig_df = hem_df[hem_df['Screening Site'] == site]
+    fig = px.bar(fig_df, x='Hemolysis', y='count', color='Visit', title=site,barmode="group")
+    fig.update_xaxes(categoryorder='category ascending')
+    return fig
+
 def make_hemolysis(df):
     hem_degrees = count_hemolysis_records(df)
+
     hemolysis = html.Div([
         html.H3('Hemolysis Data'),
+        html.Div([
+            dcc.Graph(id='graph'+site, figure = make_hemolysis_fig(hem_degrees, site)) for site in hem_degrees['Screening Site'].unique()
+        ]),
         build_datatable(hem_degrees,'table_hem_degrees'),
 
         dcc.Markdown('''
@@ -117,12 +135,19 @@ def make_hemolysis(df):
 # Protcol Deviations
 # ----------------------------------------------------------------------------
 
-deviations = dcc.Markdown('''
-**Protocol Deviations**
-Table of counts of protocol deviations by type, by site/timepoint
+def make_deviations(df):
+    deviations_df = get_deviations(df)
+    deviations = html.Div([
+        html.H3('Protocol Deviations'),
+        build_datatable(deviations_df,'table_deviations'),
+        dcc.Markdown('''
+        **Protocol Deviations**
+        Table of counts of protocol deviations by type, by site/timepoint
 
-Flags: Pull complete data for all samples with any protocol deviation
+        Flags: Pull complete data for all samples with any protocol deviation
 
-Purpose: Track protocol issues to allow early intervention if recurring problems are occurring
-    '''
-    ,style={"white-space": "pre"}),
+        Purpose: Track protocol issues to allow early intervention if recurring problems are occurring
+            '''
+            ,style={"white-space": "pre"}),
+        ])
+    return deviations
